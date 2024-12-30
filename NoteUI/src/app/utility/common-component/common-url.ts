37cdf5e82eeb6a9aa2dabfdf547e730984f2b9ad
rkumar <rkuma@logilite.com>
2024-12-30 12:10:38 +0530

    url update

--- NoteUI/src/app/utility/common-component/common-url.ts
+++ NoteUI/src/app/utility/common-component/common-url.ts
@@ -16,26 +16,3 @@ export const FORGOT_PASSWORD: string = URL + "/api/user/forgotPassword";
 
 
 
-
-export const GET_TABLE_DATA: string = URL + "/editor/getSurveyCategoryData";
-export const GET_QC_USERS: string = URL + "/editor/getQcUsers";
-export const GET_ACCESS_DATA: string = URL + "/editor/getAccessbyDepartmentUser";
-export const ACCEPT_DATA: string = URL + "/editor/accept";
-export const INSERT_QC_LOG: string = URL + "/editor/insertQcLog";
-export const GET_EXTENT: string = URL + "/editor/getExtent";
-export const GET_IMAGE: string = URL + "/getByteArrayget";
-export const GET_DASHBORD_DATA: string = URL + "/editor/getDashbordData";
-export const GET_VIDEO: string = URL + "/editor/getByteArrayForVideo";
-export const ZIP_FILE_DOWNLOAD: string = 'http://192.168.20.56:8092/api/layers/exportShapeFile'
-
-
-
-
-
-
-
-export const API_AUTH_AUTHENTICATE: string =  URL + "/auth/login";
-export const API_FORGET_PASSWORD: string = URL + "/forgotPassword";
-export const API_AUTH_CHANGE_PASSWORD: string =URL + "/changePassword";
-export const API_AUTH_AUTHENTICATE_RESET: string = URL + "/auth/resetPassword";
-export const API_AUTH_CHECK_EXPIRATION: string = URL + "/auth/checkTokenExpiration";
